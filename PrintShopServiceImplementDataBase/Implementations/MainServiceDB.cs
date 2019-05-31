4eb741dd2eed7f4945913676322c70136499416a
Nakorr <Nakorr777@gmail.com>
2019-05-31 14:52:41 +0400

    Lab6.1

--- PrintShopServiceImplementDataBase/Implementations/MainServiceDB.cs
+++ PrintShopServiceImplementDataBase/Implementations/MainServiceDB.cs
@@ -27,25 +27,25 @@ namespace PrintShopServiceImplementDataBase.Implementations
                 Id = rec.Id,
                 CustomerId = rec.CustomerId,
                 PrintId = rec.PrintId,
-                ImplementerId = rec.Implementer.Id,
+                ImplementerId = rec.ImplementerId,
                 DateCreate = SqlFunctions.DateName("dd", rec.DateCreate) + " " +
-            SqlFunctions.DateName("mm", rec.DateCreate) + " " +
-            SqlFunctions.DateName("yyyy", rec.DateCreate),
-                DateImplement = rec.DateImplement == null ? "" :
-            SqlFunctions.DateName("dd",
-           rec.DateImplement.Value) + " " +
-            SqlFunctions.DateName("mm",
-           rec.DateImplement.Value) + " " +
-            SqlFunctions.DateName("yyyy",
-           rec.DateImplement.Value),
+                 SqlFunctions.DateName("mm", rec.DateCreate) + " " +
+                 SqlFunctions.DateName("yyyy", rec.DateCreate),
+                                DateImplement = rec.DateImplement == null ? "" :
+                 SqlFunctions.DateName("dd",
+                rec.DateImplement.Value) + " " +
+                 SqlFunctions.DateName("mm",
+                rec.DateImplement.Value) + " " +
+                 SqlFunctions.DateName("yyyy",
+                rec.DateImplement.Value),
                 Status = rec.Status.ToString(),
                 Count = rec.Count,
                 Sum = rec.Sum,
                 CustomerFIO = rec.Customer.CustomerFIO,
+                PrintName = rec.Print.PrintName,
                 ImplementerName = rec.Implementer.ImplementerFIO
-
             })
-            .ToList();
+ .ToList();
             return result;
         }
         public void CreateIndent(IndentBindingModel model)
